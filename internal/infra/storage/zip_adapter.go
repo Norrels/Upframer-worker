f818bfe5a301643ff3fafc28c13805967fdcb3cf
Matheus Zacarias <matheuus412@gmail.com>
2025-09-26 19:50:20 -0300

    refactor: create a zip adapter

---
+++ internal/infra/storage/zip_adapter.go
@@ -0,0 +1,54 @@
+package storage
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type ZipAdapter struct{}
+
+func NewZipAdapter() *ZipAdapter {
+	return &ZipAdapter{}
+}
+
+func (z *ZipAdapter) CreateZipFile(sourceDir, zipPath string) error {
+	zipFile, err := os.Create(zipPath)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	zipWriter := zip.NewWriter(zipFile)
+	defer zipWriter.Close()
+
+	return filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(sourceDir, path)
+		if err != nil {
+			return err
+		}
+
+		zipEntry, err := zipWriter.Create(relPath)
+		if err != nil {
+			return err
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+
+		_, err = io.Copy(zipEntry, file)
+		return err
+	})
+}
